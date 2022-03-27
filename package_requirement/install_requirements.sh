e53150e40a96c47b37e089ca8085588816c94248
unknown <krishnamohan0602@gmail.com>
2022-03-28 01:41:49 +0530

    requirements changed

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -2,10 +2,10 @@
 python --version
 pip install --upgrade azure-cli
 pip install --upgrade azureml-sdk
-pip install -r requirements.txt
+# pip install -r requirements.txt
 pip install numpy
 pip install matplotlib
 pip install scikit-learn
 pip install pandas
 pip install azureml-core
-pip install --upgrade azureml-dataset-runtime
+pip install azureml-dataset-runtime
