ecf8476102ba8a7d503e7c1db3953160e41a6761
unknown <krishnamohan0602@gmail.com>
2022-03-28 12:01:54 +0530

    send data file

--- training/send_session_data.py
+++ training/send_session_data.py
@@ -12,12 +12,12 @@ def run_sample():
         container_name = 'sessionbatterydata'
     
         ## create container name
-        # blob_service_client.create_container(container_name)
+        blob_service_client.create_container(container_name)
 
         ## set the permission so the blobs are public
-        # blob_service_client.set_container_acl(
-        #                         container_name, public_access=PublicAccess.Container
-        # )
+        blob_service_client.set_container_acl(
+                                container_name, public_access=PublicAccess.Container
+        )
 
         path_remove = "D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training"
         local_path = 'D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training/ForSessionTraining'
@@ -30,19 +30,59 @@ def run_sample():
                     file_path_on_local = os.path.join(r, file)
                     file_path_on_local = file_path_on_local.replace('\\', "/")
 
+                    # print(file_path_on_local)
+                    # print(file_path_on_azure)
+
                     blob_service_client.create_blob_from_path(container_name,
                                                             file_path_on_azure,
                                                             file_path_on_local
                                                             )
         
-        print('List of Blobs')
-        generator = blob_service_client.list_blobs(container_name)
-        for blob in generator:
-            print("\t Blob name: " + blob.name)
+        # print('List of Blobs')
+        # generator = blob_service_client.list_blobs(container_name)
+        # for blob in generator:
+        #     print("\t Blob name: " + blob.name)
 
+
+        # temp_file_local = 
+        # temp_file_azure = 
+        # blob_service_client.create_blob_from_path(container_name,
+                                                # file_path_on_azure,
+                                                # temp_file_local
+                                                # )
         print('Upload compelete')
-        
 
+    except Exception as e:
+        print(e)
+
+
+def delete_blob():
+    try:
+        account_name = 'sessiontrainingstorage'
+        account_key = 'HS1Z8fmCdNHt314wk+CjpC7DlIFQfRp5AxbqWO+ZCH6c0zSkVMJFPlri8YOIoFiaKRIPo+mQ2BvwjAac5TXOIw=='
+        blob_service_client = BlockBlobService(account_name, account_key)
+
+        container_name = 'sessionbatterydata'
+
+        # container_client = blob_service_client.get_container_client(container_name)
+        ## create container name
+        # blob_service_client.create_container(container_name)
+
+        ## set the permission so the blobs are public
+        # blob_service_client.set_container_acl(
+        #                         container_name, public_access=PublicAccess.Container
+        # )
+        
+        # print('List of Blobs')
+        # generator = blob_service_client.list_blobs(container_name)
+        # for blob in generator:
+        #     print("\t Blob name: " + blob.name)
+        #     blob_service_client.delete_blob(container_name, blob)
+
+        # my_blobs = container_client.list_blobs(name_starts_with="my_blob")
+        # container_client.delete_blobs(*generator)
+
+        blob_service_client.delete_container(container_name)
 
     except Exception as e:
         print(e)
@@ -50,6 +90,7 @@ def run_sample():
 
 # Main method
 if __name__ == '__main__':
+    # delete_blob()
     run_sample()
 
 
