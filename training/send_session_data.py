95b38c102fe86123228577c78cab6e303804adae
unknown <krishnamohan0602@gmail.com>
2022-04-14 19:21:33 +0530

    session training updated

--- training/send_session_data.py
+++ training/send_session_data.py
@@ -1,23 +1,109 @@
 import os
 import uuid
 import sys
-from azure.storage.blob import BlockBlobService, PublicAccess
+import time
+# from azure.storage.blob import BlockBlobService, PublicAccess
+from azure.storage.blob import BlobServiceClient, PublicAccess
 
-def run_sample():
+def connector_fn():
     try:
         account_name = 'sessiontrainingstorage'
+        account_url = 'https://'+account_name+'.blob.core.windows.net'
         account_key = 'HS1Z8fmCdNHt314wk+CjpC7DlIFQfRp5AxbqWO+ZCH6c0zSkVMJFPlri8YOIoFiaKRIPo+mQ2BvwjAac5TXOIw=='
-        blob_service_client = BlockBlobService(account_name, account_key)
+        blob_service_client = BlobServiceClient(account_url, account_key)
 
         container_name = 'sessionbatterydata'
     
         ## create container name
-        blob_service_client.create_container(container_name)
+        # blob_service_client.create_container(container_name)
 
         ## set the permission so the blobs are public
-        blob_service_client.set_container_acl(
-                                container_name, public_access=PublicAccess.Container
-        )
+        # blob_service_client.set_container_acl(
+        #                         container_name, public_access=PublicAccess.Container
+        # )
+
+    except Exception as e:
+        print(e)
+
+    return blob_service_client, container_name
+
+
+def checkAndDeleteData(blob_service_client, container_name):
+    container_client = blob_service_client.get_container_client(container_name)
+    print('List of Blobs')
+    generator = container_client.list_blobs()
+    for blob in generator:
+        # print("\t Blob name: " + blob.name)
+        container_client.delete_blob(blob)
+    
+    print('Delete Complete')   
+
+
+def Delay(t):
+    print('in delay')
+    time.sleep(t)
+    print('delay complete')
+
+
+def sendNextSessionData(blob_service_client, container_name):
+    container_client = blob_service_client.get_container_client(container_name)
+    path_remove = "D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training"
+    local_path = 'D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training/ForSessionTraining'
+
+    for r,d,f in os.walk(local_path):
+        if f:
+            for file in f:
+                file_path_on_azure = os.path.join(r, file).replace(path_remove, "")
+                file_path_on_azure = file_path_on_azure.replace('\\', "/")
+                file_path_on_local = os.path.join(r, file)
+                file_path_on_local = file_path_on_local.replace('\\', "/")
+
+                # print(file_path_on_local)
+                # print(file_path_on_azure)
+
+                # blob_service_client.create_blob_from_path(container_name,
+                #                                         file_path_on_azure,
+                #                                         file_path_on_local
+                #                                         )
+
+                blob_client = container_client.get_blob_client(file_path_on_azure)
+
+                with open(file_path_on_local, 'rb') as data:
+                    blob_client.upload_blob(data)
+
+    print('Upload Complete')
+
+
+def send_data(blob_service_client, container_name):
+    
+    for i in range(2):
+        print('Running Iteration num -- ', i)
+        checkAndDeleteData(blob_service_client, container_name)
+        Delay(60)
+        sendNextSessionData(blob_service_client, container_name)
+
+        Delay(60*15)
+        print('\n')
+
+
+def run_sample():
+    try:
+        account_name = 'sessiontrainingstorage'
+        account_url = 'https://sessiontrainingstorage.blob.core.windows.net'
+        account_key = 'HS1Z8fmCdNHt314wk+CjpC7DlIFQfRp5AxbqWO+ZCH6c0zSkVMJFPlri8YOIoFiaKRIPo+mQ2BvwjAac5TXOIw=='
+        # blob_service_client = BlockBlobService(account_name, account_key)
+        blob_service_client = BlobServiceClient(account_url, account_key)
+
+        container_name = 'sessionbatterydata'
+    
+        ## create container name
+        # blob_service_client.create_container(container_name)
+        container_client = blob_service_client.get_container_client(container_name)
+
+        ## set the permission so the blobs are public
+        # blob_service_client.set_container_acl(
+        #                         container_name, public_access=PublicAccess.Container
+        # )
 
         path_remove = "D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training"
         local_path = 'D:/OneDrive - IIT Kanpur/Thesis/Thesis_Git/Thesis/training/ForSessionTraining'
@@ -33,10 +119,15 @@ def run_sample():
                     # print(file_path_on_local)
                     # print(file_path_on_azure)
 
-                    blob_service_client.create_blob_from_path(container_name,
-                                                            file_path_on_azure,
-                                                            file_path_on_local
-                                                            )
+                    # blob_service_client.create_blob_from_path(container_name,
+                    #                                         file_path_on_azure,
+                    #                                         file_path_on_local
+                    #                                         )
+
+                    blob_client = container_client.get_blob_client(file_path_on_azure)
+
+                    with open(file_path_on_local, 'rb') as data:
+                        blob_client.upload_blob(data)
         
         # print('List of Blobs')
         # generator = blob_service_client.list_blobs(container_name)
@@ -59,8 +150,10 @@ def run_sample():
 def delete_blob():
     try:
         account_name = 'sessiontrainingstorage'
+        account_url = 'https://sessiontrainingstorage.blob.core.windows.net'
         account_key = 'HS1Z8fmCdNHt314wk+CjpC7DlIFQfRp5AxbqWO+ZCH6c0zSkVMJFPlri8YOIoFiaKRIPo+mQ2BvwjAac5TXOIw=='
-        blob_service_client = BlockBlobService(account_name, account_key)
+        # blob_service_client = BlockBlobService(account_name, account_key)
+        blob_service_client = BlobServiceClient(account_url, account_key)
 
         container_name = 'sessionbatterydata'
 
@@ -73,16 +166,19 @@ def delete_blob():
         #                         container_name, public_access=PublicAccess.Container
         # )
         
-        # print('List of Blobs')
-        # generator = blob_service_client.list_blobs(container_name)
-        # for blob in generator:
-        #     print("\t Blob name: " + blob.name)
-        #     blob_service_client.delete_blob(container_name, blob)
+        container_client = blob_service_client.get_container_client(container_name)
+
+        print('List of Blobs')
+        generator = container_client.list_blobs()
+        for blob in generator:
+            print("\t Blob name: " + blob.name)
+            # blob_service_client.delete_blob(container_name, blob)
+            container_client.delete_blob(blob)
 
         # my_blobs = container_client.list_blobs(name_starts_with="my_blob")
         # container_client.delete_blobs(*generator)
 
-        blob_service_client.delete_container(container_name)
+        # blob_service_client.delete_container(container_name)
 
     except Exception as e:
         print(e)
@@ -91,7 +187,11 @@ def delete_blob():
 # Main method
 if __name__ == '__main__':
     # delete_blob()
-    run_sample()
+    # run_sample()
+
+    blob_service_client, container_name = connector_fn()
+
+    send_data(blob_service_client, container_name)
 
 
 
